1a2e28f5080e04201516051ea0912395b18c0919
Slava Chalnev <slava.chalnev@gmail.com>
2024-03-28 14:30:03 +1100

    Display patterns

---
+++ utils.py
@@ -0,0 +1,95 @@
+import torch as t
+from torch import Tensor
+from typing import List, Union, Optional
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import numpy as np
+import re
+from typing import Dict
+import pandas as pd
+from jaxtyping import Float
+import einops
+
+
+### Copied from ARENA ###
+def to_numpy(tensor):
+    """
+    Helper function to convert a tensor to a numpy array. Also works on lists, tuples, and numpy arrays.
+    """
+    if isinstance(tensor, np.ndarray):
+        return tensor
+    elif isinstance(tensor, (list, tuple)):
+        array = np.array(tensor)
+        return array
+    elif isinstance(tensor, (t.Tensor, t.nn.parameter.Parameter)):
+        return tensor.detach().cpu().numpy()
+    elif isinstance(tensor, (int, float, bool, str)):
+        return np.array(tensor)
+    else:
+        raise ValueError(f"Input to to_numpy has invalid type: {type(tensor)}")
+
+
+update_layout_set = {"xaxis_range", "yaxis_range", "hovermode", "xaxis_title", "yaxis_title", "colorbar", "colorscale", "coloraxis", "title_x", "bargap", "bargroupgap", "xaxis_tickformat", "yaxis_tickformat", "title_y", "legend_title_text", "xaxis_showgrid", "xaxis_gridwidth", "xaxis_gridcolor", "yaxis_showgrid", "yaxis_gridwidth", "yaxis_gridcolor", "showlegend", "xaxis_tickmode", "yaxis_tickmode", "margin", "xaxis_visible", "yaxis_visible", "bargap", "bargroupgap", "coloraxis_showscale", "xaxis_tickangle"}
+
+### Copied from ARENA ###
+def imshow(tensor: t.Tensor, renderer=None, **kwargs):
+    kwargs_post = {k: v for k, v in kwargs.items() if k in update_layout_set}
+    kwargs_pre = {k: v for k, v in kwargs.items() if k not in update_layout_set}
+    facet_labels = kwargs_pre.pop("facet_labels", None)
+    border = kwargs_pre.pop("border", False)
+    return_fig = kwargs_pre.pop("return_fig", False)
+    text = kwargs_pre.pop("text", None)
+    xaxis_tickangle = kwargs_post.pop("xaxis_tickangle", None)
+    static = kwargs_pre.pop("static", False)
+    if "color_continuous_scale" not in kwargs_pre:
+        kwargs_pre["color_continuous_scale"] = "RdBu"
+    if "color_continuous_midpoint" not in kwargs_pre:
+        kwargs_pre["color_continuous_midpoint"] = 0.0
+    if "margin" in kwargs_post and isinstance(kwargs_post["margin"], int):
+        kwargs_post["margin"] = dict.fromkeys(list("tblr"), kwargs_post["margin"])
+    fig = px.imshow(to_numpy(tensor), **kwargs_pre).update_layout(**kwargs_post)
+    if facet_labels:
+        # Weird thing where facet col wrap means labels are in wrong order
+        if "facet_col_wrap" in kwargs_pre:
+            facet_labels = reorder_list_in_plotly_way(facet_labels, kwargs_pre["facet_col_wrap"])
+        for i, label in enumerate(facet_labels):
+            fig.layout.annotations[i]['text'] = label
+    if border:
+        fig.update_xaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
+        fig.update_yaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
+    if text:
+        if tensor.ndim == 2:
+            # if 2D, then we assume text is a list of lists of strings
+            assert isinstance(text[0], list)
+            assert isinstance(text[0][0], str)
+            text = [text]
+        else:
+            # if 3D, then text is either repeated for each facet, or different
+            assert isinstance(text[0], list)
+            if isinstance(text[0][0], str):
+                text = [text for _ in range(len(fig.data))]
+        for i, _text in enumerate(text):
+            fig.data[i].update(
+                text=_text, 
+                texttemplate="%{text}", 
+                textfont={"size": 12}
+            )
+    # Very hacky way of fixing the fact that updating layout with new tickangle only applies to first facet by default
+    if xaxis_tickangle is not None:
+        n_facets = 1 if tensor.ndim == 2 else tensor.shape[0]
+        for i in range(1, 1+n_facets):
+            xaxis_name = "xaxis" if i == 1 else f"xaxis{i}"
+            fig.layout[xaxis_name]["tickangle"] = xaxis_tickangle
+    return fig if return_fig else fig.show(renderer=renderer, config={"staticPlot": static})
+
+### Copied from ARENA ###
+def reorder_list_in_plotly_way(L: list, col_wrap: int):
+    '''
+    Helper function, because Plotly orders figures in an annoying way when there's column wrap.
+    '''
+    L_new = []
+    while len(L) > 0:
+        L_new.extend(L[-col_wrap:])
+        L = L[:-col_wrap]
+    return L_new
